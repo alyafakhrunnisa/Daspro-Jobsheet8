91861bb2f11043f1d799f599c05649ae06a65ea0
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2025-10-29 10:54:35 +0700

    Jumlah Kuadrat

--- src/NilaiKelompok02.java
+++ src/NilaiKelompok02.java
@@ -1,46 +1,39 @@
 import java.util.Scanner;
 public class NilaiKelompok02 {
     public static void main(String[] args) {
-        //Buat deklarasi Scanner dengan nama variabel sc di dalam fungsi main()
         Scanner sc = new Scanner(System.in);
+        int i, j, nilai, kelompokTertinggi=0;
+        float totalNilai, rataNilai, maxRataNilai=0;
 
-        //Deklarasi variabel sesuai pada flowchart
-        int i, j, nilai;
-        float totalNilai, rataNilai;
 
-        // Mengulang proses memasukkan nilai untuk 6 kelompok (Outer Loop menggunakan while)
+        
         i = 1;
-        while (i <= 6) { // Pengulangan untuk 6 Kelompok
-            System.out.println("\nKelompok " + i); // Tampilkan Kelompok ke-i
-
-            //totalNilai direset ke 0 sebelum memulai pengulangan inner loop 
+        while (i <= 3) { 
+            System.out.println("\nKelompok " + i);
             totalNilai = 0;
 
-            //Struktur perulangan for (inner loop) untuk memasukkan 5 nilai
-            for (j = 1; j <= 5; j++) { // Pengulangan untuk 5 Penilai
-                //Perintah untuk memasukkan 5 nilai dan menghitung total
+            
+            for (j = 1; j <= 5; j++) { 
                 System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
-                // Memastikan input adalah integer
                 while (!sc.hasNextInt()) {
                     System.out.println("Input tidak valid. Masukkan angka bulat.");
-                    sc.next(); // Buang input yang salah
+                    sc.next(); 
                     System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
                 }
                 nilai = sc.nextInt();
-                totalNilai += nilai; // totalNilai = totalNilai + nilai;
+                totalNilai += nilai; 
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+            if (rataNilai > maxRataNilai) {
+                maxRataNilai = rataNilai;      
+                kelompokTertinggi = i;         
             }
 
-            //Hitung rata-rata
-            //Menggunakan (float)5 agar pembagian menghasilkan float, karena totalNilai juga float
-            rataNilai = totalNilai / 5;
-
-            //Tampilkan hasil (output rataNilai)
-            //Tambahkan narasi keterangan untuk kebutuhan masukan dan luaran
-            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
-
-            // Increment i (i++) untuk lanjut ke kelompok berikutnya
+            
             i++;
         }
+        System.out.println("Kelompok dengan rata-rata nilai tertinggi adalah " + "Kelompok " + kelompokTertinggi + " dengan rata-rata " + maxRataNilai);
 
         
 }
--- src/Star02.java
+++ src/Star02.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
 public class Star02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris: ");
+        int n = sc.nextInt();
+
+        for (int i =1; i < n; i++) {
+            System.out.print("*");
+        }
+    }
     
 }
